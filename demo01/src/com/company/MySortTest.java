120eaa3d77162e7b4c1dd5f640b6108b9d31530e
Taitana Yumee <taitana.y@kbtg.tech>
2021-06-17 17:11:19 +0700

    add 2 sorted value test case

--- demo01/src/com/company/MySortTest.java
+++ demo01/src/com/company/MySortTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class MySortTest {
     @Test
     @DisplayName("Sorting with empty list")
-    public void emptySort(){
+    public void emptyValue(){
         List<Integer> expectedValue = new ArrayList<>();
         List<Integer> input = new ArrayList<>();
         MySort test = new MySort((ArrayList<Integer>) input);
@@ -21,11 +21,25 @@ class MySortTest {
 
     @Test
     @DisplayName("Sorting with 1 value list")
-    public void oneValueSort(){
+    public void oneValue(){
         List<Integer> expectedValue = new ArrayList<>();
         expectedValue.add(1);
         List<Integer> input = new ArrayList<>();
         input.add(1);
+        MySort test = new MySort((ArrayList<Integer>) input);
+        List<Integer> result = test.ascendingSort();
+        assertEquals(expectedValue, result);
+    }
+
+    @Test
+    @DisplayName("Sorting with 2 sorted value list")
+    public void twoSortedValue(){
+        List<Integer> expectedValue = new ArrayList<>();
+        expectedValue.add(1);
+        expectedValue.add(2);
+        List<Integer> input = new ArrayList<>();
+        input.add(1);
+        input.add(2);
         MySort test = new MySort((ArrayList<Integer>) input);
         List<Integer> result = test.ascendingSort();
         assertEquals(expectedValue, result);
