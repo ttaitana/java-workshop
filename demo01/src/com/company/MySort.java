5e5534df57a8af34eb655938088cc270c19b8fc2
Taitana Yumee <taitana.y@kbtg.tech>
2021-06-17 17:04:05 +0700

    test with empty list

--- demo01/src/com/company/MySort.java
+++ demo01/src/com/company/MySort.java
@@ -1,4 +1,16 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MySort {
+    List<Integer> collection;
+
+    MySort(ArrayList<Integer> collection){
+        this.collection = collection;
+    }
+
+    public ArrayList<Integer> ascendingSort(){
+        return (ArrayList<Integer>) this.collection;
+    }
 }
--- demo01/src/com/company/MySortTest.java
+++ demo01/src/com/company/MySortTest.java
@@ -1,7 +1,21 @@
 package com.company;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MySortTest {
-
+    @Test
+    @DisplayName("Sorting with empty list")
+    public void emptySort(){
+        List<Integer> expectedValue = new ArrayList<>();
+        List<Integer> input = new ArrayList<>();
+        MySort test = new MySort((ArrayList<Integer>) input);
+        List<Integer> result = test.ascendingSort();
+        assertEquals(expectedValue, result);
+    }
 }
