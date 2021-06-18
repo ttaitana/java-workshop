74a3396b4ea8eedc6ff64acd153ea440ed8b6ee8
Taitana <taitana.ym@outlook.com>
2021-06-18 17:22:48 +0700

    refactor variables name

--- hello-spring/src/main/java/workshop01/TennisGame3.java
+++ hello-spring/src/main/java/workshop01/TennisGame3.java
@@ -2,35 +2,35 @@ package workshop01;
 
 public class TennisGame3 {
 
-    private int p2;
-    private int p1;
-    private String p1N;
-    private String p2N;
+    private int player1Score;
+    private int player2Score;
+    private String player1Name;
+    private String player2Name;
 
-    public TennisGame3(String p1N, String p2N) {
-        this.p1N = p1N;
-        this.p2N = p2N;
+    public TennisGame3(String player1Number, String player2Name) {
+        this.player1Name = player1Number;
+        this.player2Name = player2Name;
     }
 
     public String getScore() {
         String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
+        if (player1Score < 4 && player2Score < 4 && !(player1Score + player2Score == 6)) {
             String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+            s = p[player1Score];
+            return (player1Score == player2Score) ? s + "-All" : s + "-" + p[player2Score];
         } else {
-            if (p1 == p2)
+            if (player1Score == player2Score)
                 return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1 - p2) * (p1 - p2) == 1) ? "Advantage " + s : "Win for " + s;
+            s = player1Score > player2Score ? player1Name : player2Name;
+            return ((player1Score - player2Score) * (player1Score - player2Score) == 1) ? "Advantage " + s : "Win for " + s;
         }
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            this.p1 += 1;
+        if ("player1".equals(playerName))
+            this.player1Score += 1;
         else
-            this.p2 += 1;
+            this.player2Score += 1;
 
     }
 
