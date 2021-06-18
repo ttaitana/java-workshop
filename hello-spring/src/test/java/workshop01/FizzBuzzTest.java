6e881bf889f2460e7eab707190503f5eb698bd39
Taitana <taitana.ym@outlook.com>
2021-06-18 14:57:32 +0700

    add 2-5 number testcase

--- hello-spring/src/test/java/workshop01/FizzBuzzTest.java
+++ hello-spring/src/test/java/workshop01/FizzBuzzTest.java
@@ -6,11 +6,43 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTest {
     @Test
-    public void fizzBuzz1(){
+    public void fizzBuzz1() {
         FizzBuzz fb = new FizzBuzz();
         String result = fb.printFizzBuzz(1);
         String expectedValue = "1";
-        assertEquals(result, expectedValue);
+        assertEquals(expectedValue, result);
+    }
+
+    @Test
+    public void fizzBuzz2() {
+        FizzBuzz fb = new FizzBuzz();
+        String result = fb.printFizzBuzz(2);
+        String expectedValue = "2";
+        assertEquals(expectedValue, result);
+    }
+
+    @Test
+    public void fizzBuzz3() {
+        FizzBuzz fb = new FizzBuzz();
+        String result = fb.printFizzBuzz(3);
+        String expectedValue = "Fizz";
+        assertEquals(expectedValue, result);
+    }
+
+    @Test
+    public void fizzBuzz4() {
+        FizzBuzz fb = new FizzBuzz();
+        String result = fb.printFizzBuzz(4);
+        String expectedValue = "4";
+        assertEquals(expectedValue, result);
+    }
+
+    @Test
+    public void fizzBuzz5() {
+        FizzBuzz fb = new FizzBuzz();
+        String result = fb.printFizzBuzz(5);
+        String expectedValue = "Buzz";
+        assertEquals(expectedValue, result);
     }
 
 }
