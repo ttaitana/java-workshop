b041c44048a51fcfc8bdfaa799340b9b950ad910
Taitana <taitana.ym@outlook.com>
2021-06-18 16:03:49 +0700

    FizzBuzz v.2

--- hello-spring/src/main/java/workshop01/FizzBuzz.java
+++ hello-spring/src/main/java/workshop01/FizzBuzz.java
@@ -1,8 +1,35 @@
 package workshop01;
 
 public class FizzBuzz {
-    public String printFizzBuzz(int number) {
+    private int number;
+
+    public String printFizzBuzzOld(int number) {
         String result = number % 3 == 0 & number % 5 == 0 ? "FizzBuzz" : number % 3 == 0 ? "Fizz" : number % 5 == 0 ? "Buzz" : Integer.toString(number);
         return result;
     }
+
+    public String printFizzBuzz(int number){
+        this.setNumber(number);
+        return this.fizzBuzzChecker();
+    }
+
+    public boolean isDividedBy(int divider){
+        return this.number % divider == 0;
+    }
+
+    public String fizzBuzzChecker(){
+        if(this.isDividedBy(15)) return "FizzBuzz";
+        if(this.isDividedBy(5)) return "Buzz";
+        if(this.isDividedBy(3)) return "Fizz";
+        return Integer.toString(this.getNumber());
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
 }
