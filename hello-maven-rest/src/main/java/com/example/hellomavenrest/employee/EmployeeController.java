63e823599ecd4684a439b533738045179c445402
Taitana <taitana.ym@outlook.com>
2021-06-21 13:42:36 +0700

    add param query

--- hello-maven-rest/src/main/java/com/example/hellomavenrest/employee/EmployeeController.java
+++ hello-maven-rest/src/main/java/com/example/hellomavenrest/employee/EmployeeController.java
@@ -2,12 +2,24 @@ package com.example.hellomavenrest.employee;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
     @GetMapping("/employee/{id}")
     public EmployeeResponse getEmployeeById(@PathVariable String id){
+        int _id = 0;
+        try{
+            _id = Integer.parseInt(id);
+        }catch (Exception e){
+            System.out.println("Invalid type of id");
+        }
+        return new EmployeeResponse(_id, "Taitana", "Yumee");
+    }
+
+    @GetMapping("/employee")
+    public EmployeeResponse getEmployeeByParams(@RequestParam String id){
         int _id = 0;
         try{
             _id = Integer.parseInt(id);
