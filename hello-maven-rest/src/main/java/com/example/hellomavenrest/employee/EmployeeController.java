ca855900e9b3e29f9dd64d1a3e9d50a75e2767af
Taitana <taitana.ym@outlook.com>
2021-06-21 11:54:30 +0700

    id validation

--- hello-maven-rest/src/main/java/com/example/hellomavenrest/employee/EmployeeController.java
+++ hello-maven-rest/src/main/java/com/example/hellomavenrest/employee/EmployeeController.java
@@ -7,7 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EmployeeController {
     @GetMapping("/employee/{id}")
-    public EmployeeResponse getEmployeeById(@PathVariable int id){
-        return new EmployeeResponse(id, "Taitana", "Yumee");
+    public EmployeeResponse getEmployeeById(@PathVariable String id){
+        int _id = 0;
+        try{
+            _id = Integer.parseInt(id);
+        }catch (Exception e){
+            System.out.println("Invalid type of id");
+        }
+        return new EmployeeResponse(_id, "Taitana", "Yumee");
     }
 }
