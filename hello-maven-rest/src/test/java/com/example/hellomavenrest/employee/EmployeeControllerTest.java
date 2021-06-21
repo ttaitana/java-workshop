575223d5724468118604e215a1b390d544fff422
Taitana <taitana.ym@outlook.com>
2021-06-21 14:11:12 +0700

    add employee testing

---
+++ hello-maven-rest/src/test/java/com/example/hellomavenrest/employee/EmployeeControllerTest.java
@@ -0,0 +1,27 @@
+package com.example.hellomavenrest.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.*;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class EmployeeControllerTest {
+    @Test
+    public void callAPIWithPathVariable(@Autowired TestRestTemplate restTemplate){
+        EmployeeResponse response = restTemplate.getForObject("/employee/123", EmployeeResponse.class);
+        assertEquals(123, response.getId());
+        assertEquals("Taitana", response.getFname());
+        assertEquals("Yumee", response.getLname());
+    }
+
+    public void callAPIWithQueryVariable(@Autowired TestRestTemplate restTemplate){
+        EmployeeResponse response = restTemplate.getForObject("/employee?id=12", EmployeeResponse.class);
+        assertEquals(12, response.getId());
+        assertEquals("Taitana", response.getFname());
+        assertEquals("Yumee", response.getLname());
+    }
+}
