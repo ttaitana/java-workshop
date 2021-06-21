e8bcc08541b202af9fe97fd6a4c073db7756964b
Taitana <taitana.ym@outlook.com>
2021-06-21 14:29:37 +0700

    fix testing

--- hello-maven-rest/src/main/java/com/example/hellomavenrest/employee/EmployeeResponse.java
+++ hello-maven-rest/src/main/java/com/example/hellomavenrest/employee/EmployeeResponse.java
@@ -1,13 +1,25 @@
 package com.example.hellomavenrest.employee;
 
+import java.util.Objects;
+
 public class EmployeeResponse {
     private String fname;
     private String lname;
     private int id;
 
     @Override
-    public String toString() {
-        return String.format("id: %d, %s %s", this.getId(), this.getFname(), this.getLname());
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof EmployeeResponse)) return false;
+        EmployeeResponse that = (EmployeeResponse) o;
+        return getId() == that.getId() &&
+                Objects.equals(getFname(), that.getFname()) &&
+                Objects.equals(getLname(), that.getLname());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getFname(), getLname(), getId());
     }
 
     public EmployeeResponse() {
