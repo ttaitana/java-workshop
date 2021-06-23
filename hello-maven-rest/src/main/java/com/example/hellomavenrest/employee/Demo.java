be4f73c98a7be4fca9ca2148df736d831da12b8a
Taitana <taitana.ym@outlook.com>
2021-06-23 16:55:49 +0700

    loop thrad

--- hello-maven-rest/src/main/java/com/example/hellomavenrest/employee/Demo.java
+++ hello-maven-rest/src/main/java/com/example/hellomavenrest/employee/Demo.java
@@ -2,12 +2,22 @@ package com.example.hellomavenrest.employee;
 
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Demo {
     public static void main(String[] args) {
         Task t1 = new Task("Task 1");
         t1.start();
         Task t2 = new Task("Task 2");
         t2.start();
+
+        ExecutorService service = Executors.newFixedThreadPool(10);
+
+        for (int i = 0; i < 2; i++){
+            service.execute(new Task("Task " + i));
+        }
+
         System.out.println("Task t1 with id : " + t1.getId());
         System.out.println("Task t2 with id : " + t2.getId());
         System.out.println("Main thread id : " + Thread.currentThread().getId());
