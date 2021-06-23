ff7af79ff8f0002602b8fb44cbc2ad4063562f1f
Taitana <taitana.ym@outlook.com>
2021-06-23 16:47:41 +0700

    learning thread

--- hello-maven-rest/src/main/java/com/example/hellomavenrest/employee/Demo.java
+++ hello-maven-rest/src/main/java/com/example/hellomavenrest/employee/Demo.java
@@ -2,6 +2,28 @@ package com.example.hellomavenrest.employee;
 
 import org.springframework.stereotype.Component;
 
-@Component
 public class Demo {
+    public static void main(String[] args) {
+        Task t1 = new Task("Task 1");
+        t1.start();
+        Task t2 = new Task("Task 2");
+        t2.start();
+        System.out.println("Task t1 with id : " + t1.getId());
+        System.out.println("Task t2 with id : " + t2.getId());
+        System.out.println("Main thread id : " + Thread.currentThread().getId());
+    }
+}
+
+class Task extends Thread{
+
+    private String name;
+
+    public Task(String name){
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Task Id : " + this.getId());
+    }
 }
